479ba5b5e1b068c79cfe098aca464782767c773f
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-09-14 13:31:00 +0100

    [#16] - Implement JSON loggin

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from sanic import Sanic
+from sanic.log import logger, LOGGING_CONFIG_DEFAULTS
+from sanic.request import Request as SanicRequest
+from sanic.response import BaseHTTPResponse as SanicResponse
 
 from config.loader import load_config
 from config.entities import Config
@@ -6,16 +9,35 @@ from config.entities import Config
 from application.ping.controllers import bp as ping_blueprint
 from application.ping.manager import PingManager
 
-app = Sanic("nlp")
-app.blueprint(ping_blueprint)
 config: Config = load_config()
 
+LOGGING_CONFIG_DEFAULTS["formatters"] = {
+    "generic": {
+        "class": "sanic.logging.formatter.JSONFormatter"
+    },
+    "access": {
+        "class": "sanic.logging.formatter.JSONFormatter"
+    }
+}
+
+app = Sanic("nlp")
+
+app.blueprint(ping_blueprint)
 app.ext.add_dependency(PingManager)
 
 app.ext.openapi.describe(
     "Natural Language Processing API",
     version="0.1"
 )
+
+@app.middleware("request")
+async def callback_request(request: SanicRequest) -> None:
+    logger.info(f"Request {request.path} received")
+
+
+@app.middleware("response")
+async def callback_response(request: SanicRequest, response: SanicResponse) -> None:
+    logger.info(f"Request {request.path} processing finished")
 
 
 if __name__ == "__main__":
