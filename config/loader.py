5b9b03070e579e612ee233c1511d44122b090880
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-09-26 18:55:25 +0100

    [#38] - remove toml config in favour of env vars

--- config/loader.py
+++ config/loader.py
@@ -1,8 +1,2 @@
-from toml import load
-from .entities import Config, ServerConfig
-
-
 def load_config():
-    conf = load("./config/application.toml")
-    server_conf: ServerConfig = ServerConfig(**conf["server"])
-    return Config(server=server_conf)
+    ...
